7cf5757197dfc18fde240ea02d48df81dd389622
Bv-cyber <leandro19082003@gmail.com>
2025-04-02 21:44:20 -0300

    encapsulamento

--- aula1po/aula1.js
+++ aula1po/aula1.js
@@ -3,18 +3,37 @@ const prompt = require(`prompt-sync`)();
 
 
 class Cliente {
-   nome;
+   // # -> priva o dado 
+   #nome; //atributo
    idade;
    cpf;
    agencia;
+
+  getDescricao(){
+   return "Nome : " + this.#nome + " , idade: " + this.idade;
+
+
+  }
+      
+
+    getnome(){
+         return this.#nome;
+   }
+    setNOME(value){
+        this.#nome = value;
+
+   }nade 
 }
 
 let cliente = new Cliente();
 
-cliente.nome = prompt("digite o nome do cliente");
+cliente.setNOME();
+
+cliente.setNOME(prompt("digite o nome do cliente"));
 cliente.idade = 21;
 cliente.cpf = "111.111.111-22"
 cliente.agencia = 10101010;
 
 console.log(cliente);
+console.log(cliente.getdexcricao());
 
